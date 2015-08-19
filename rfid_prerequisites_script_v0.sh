96545e0fa6da71974d3bfdc0d11a0dbccf306cd2
Jonas Groß <gross.jonas@gmail.com>
2015-08-19 20:06:12 +0200

    make dirs for ld

--- rfid_prerequisites_script_v0.sh
+++ rfid_prerequisites_script_v0.sh
@@ -2,6 +2,10 @@
 
 sudo apt-get install libusb-dev libpcsclite-dev libpam0g-dev libusb-0.1-4 libpcsclite1 libccid pcscd libssl-dev kdelibs5-dev autoconf libtool libc6-dbg g++
 
+sudo su -c 'mkdir -p /usr/local/lib'
+sudo su -c 'mkdir -p /usr/local/lib/security'
+sudo su -c 'mkdir -p /usr/local/lib/nfc-eventd/modules'
+
 sudo su -c 'echo /usr/local/lib >> /etc/ld.so.conf'
 sudo su -c 'echo /usr/local/lib/security >> /etc/ld.so.conf'
 sudo su -c 'echo /usr/local/lib/nfc-eventd/modules >> /etc/ld.so.conf'
